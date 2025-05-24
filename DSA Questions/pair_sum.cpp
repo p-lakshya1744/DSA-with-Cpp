021b82dd77056b313eb1ee74ad1153968f94470e
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-05-25 00:42:35 +0530

    update

--- DSA Questions/pair_sum.cpp
+++ DSA Questions/pair_sum.cpp
@@ -13,6 +13,29 @@ Output: 1, 2
 
 // This is the brute force approach with time complexity O(n^2)
 
+// # include <iostream>
+// using namespace std;
+
+// int main(){
+//     int nums[] = {2, 7, 11, 15};
+//     int size = sizeof(nums)/sizeof(nums[0]);
+//     int target = 18;
+
+//     for(int i=0 ; i<size ; i++){
+//         for(int j=i+1 ; j<size ; j++){
+//             if(nums[i]+nums[j]==target){
+//                 cout << i << "," << j << endl;
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+
+// This is the best approach using two-pointers with time complexity O(n)
+
 # include <iostream>
 using namespace std;
 
@@ -21,13 +44,22 @@ int main(){
     int size = sizeof(nums)/sizeof(nums[0]);
     int target = 18;
 
-    for(int i=0 ; i<size ; i++){
-        for(int j=i+1 ; j<size ; j++){
-            if(nums[i]+nums[j]==target){
-                cout << i << "," << j << endl;
-            }
+    int start = 0;
+    int end = size-1;
+
+    while(start<end){
+        if(nums[start]+nums[end]>target){
+            end--;
+        }
+        else if(nums[start]+nums[end]<target){
+            start++;
+        }
+        else{
+            cout << start << "," << end << endl;
         }
     }
+
+    cout << start << "," << end << endl;
 
     return 0;
 }
