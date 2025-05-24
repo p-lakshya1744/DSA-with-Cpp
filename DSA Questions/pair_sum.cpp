05cc72006894fe7886f55d552b56a62e5af92727
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-05-25 00:20:20 +0530

    update

---
+++ DSA Questions/pair_sum.cpp
@@ -0,0 +1,33 @@
+//Return pair of elements in a sorted array whose sum is equal to given target
+
+/*
+
+For Example, 
+Input:
+nums[] = {2, 7, 11, 15}
+target = 18
+
+Output: 1, 2
+
+*/
+
+// This is the brute force approach with time complexity O(n^2)
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    int nums[] = {2, 7, 11, 15};
+    int size = sizeof(nums)/sizeof(nums[0]);
+    int target = 18;
+
+    for(int i=0 ; i<size ; i++){
+        for(int j=i+1 ; j<size ; j++){
+            if(nums[i]+nums[j]==target){
+                cout << i << "," << j << endl;
+            }
+        }
+    }
+
+    return 0;
+}
