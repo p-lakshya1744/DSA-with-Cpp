1e163095c8a01ee34be6dc0161ce401fb881a910
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-13 01:05:24 +0530

    update

--- Trees/implementation_of_trees.cpp
+++ Trees/implementation_of_trees.cpp
@@ -1,7 +1,59 @@
-// This program performs the implementation of a binary tree
+// This program performs the two kinds of implementations of a binary tree
+
+// # include <iostream>
+// # include <queue>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* left;
+//     Node* right;
+
+//     Node(int val){
+//         data = val;
+//         left = right = NULL;
+//     }
+// };
+
+// int main(){
+
+//     int x;
+//     cout << "Enter the root element: " << endl;
+//     cin >> x;
+
+//     int first, second;
+
+//     queue <Node*> q;
+
+//     // Build the tree
+//     Node* root = new Node(x);
+//     q.push(root);
+
+//     while(!(q.empty())){
+//         Node* temp = q.front();
+//         q.pop();
+
+//         cin >> first;
+//         if(first!=-1){
+//             temp -> left = new Node(first);
+//             q.push(temp -> left);
+//         }
+
+//         cin >> second;
+//         if(second!=-1){
+//             temp -> right = new Node(second);
+//             q.push(temp -> right);
+//         }
+//     }
+    
+
+//     return 0;
+// }
+
+
 
 # include <iostream>
-# include <queue>
 using namespace std;
 
 class Node{
@@ -10,43 +62,30 @@ class Node{
     Node* left;
     Node* right;
 
-    Node(int val){
+    Node (int val){
         data = val;
         left = right = NULL;
     }
 };
 
-int main(){
-
+Node* binaryTree(){
     int x;
-    cout << "Enter the root element: " << endl;
     cin >> x;
 
-    int first, second;
-
-    queue <Node*> q;
-
-    // Build the tree
     Node* root = new Node(x);
-    q.push(root);
 
-    while(!(q.empty())){
-        Node* temp = q.front();
-        q.pop();
+    Node* temp = root;
+    cout << "Enter the left child: " << endl;
+    temp -> left = binaryTree(); 
+    cout << "Enter the right child: " << endl;
+    temp -> right = binaryTree();
 
-        cin >> first;
-        if(first!=-1){
-            temp -> left = new Node(first);
-            q.push(temp -> left);
-        }
+}
 
-        cin >> second;
-        if(second!=-1){
-            temp -> right = new Node(second);
-            q.push(temp -> right);
-        }
-    }
-    
+int main(){
+    cout << "Enter the root Node: " << endl;
+    Node* root;
 
+    root = binaryTree();
     return 0;
 }
