ab34330d05e54cd859fcf13106d536791aabbeb2
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-14 23:59:54 +0530

    update

--- Stacks/implementation_of_stack.cpp
+++ Stacks/implementation_of_stack.cpp
@@ -1,1 +1,15 @@
-// This program performs the implementation of stacks
+// This program performs the implementation of stacks using vectors, linked lists and C++ STL
+
+
+// Implementation using vectors
+# include <iostream>
+using namespace std;
+
+class Stack{
+    
+};
+
+int main(){
+
+    return 0;
+}
