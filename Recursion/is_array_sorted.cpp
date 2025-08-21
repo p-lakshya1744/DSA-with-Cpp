f6fa5cae330090a880a8491bf1a1562606429e4b
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-22 01:08:10 +0530

    update

--- Recursion/is_array_sorted.cpp
+++ Recursion/is_array_sorted.cpp
@@ -1,2 +1,19 @@
 // This program checks whether the given array(or vector) is sorted or not using recursive approach
 
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+bool isSorted(vector <int> &nums, int n){
+
+}
+
+int main(){
+    vector <int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = nums.size();
+
+    cout << isSorted(nums, n);
+
+    return 0;
+}
