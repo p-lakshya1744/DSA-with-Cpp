91318dda7a20ae09263e198b8c1d196d97c3cfc7
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-22 01:26:43 +0530

    update

--- Binary Search/recursive_method.cpp
+++ Binary Search/recursive_method.cpp
@@ -1,2 +1,21 @@
 // This program performs binary search in a vector in a recursive manner
 
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int binarySearch(vector <int> &nums, int target, int start, int end){
+    
+}
+
+int main(){
+    vector <int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 7;
+
+    int start = 0;
+    int end = nums.size()-1;
+
+    cout << binarySearch(nums, target, start, end) << endl;
+    return 0;
+}
