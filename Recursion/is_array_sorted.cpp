fdde145d8861d0ad5a9b7a31f6605bf2a80aa000
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-22 01:16:13 +0530

    update

--- Recursion/is_array_sorted.cpp
+++ Recursion/is_array_sorted.cpp
@@ -6,7 +6,10 @@
 using namespace std;
 
 bool isSorted(vector <int> &nums, int n){
-    
+    if(n==0 || n==1){
+        return true;
+    }
+    return nums[n-1] >= nums[n-2] && isSorted(nums, n-1);
 }
 
 int main(){
