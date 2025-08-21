8710df7e82f25bcca238fb1f0de3c89df1923b94
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-21 23:28:17 +0530

    update

--- Recursion/n_factorial.cpp
+++ Recursion/n_factorial.cpp
@@ -1,1 +1,19 @@
 // This program calculates n! using recursive approach
+
+# include <iostream>
+using namespace std;
+
+int nFactorial(int n){
+
+}
+
+int main(){
+    int n;
+
+    cout << "Enter a number: " << endl;
+    cin >> n;
+
+    cout << nFactorial(n) << endl;
+
+    return 0;
+}
