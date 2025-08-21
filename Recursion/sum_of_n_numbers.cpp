1905bd7ebf4202f7c7a4fb9893c6150fe3824cfa
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-22 00:34:43 +0530

    update

---
+++ Recursion/sum_of_n_numbers.cpp
@@ -0,0 +1,15 @@
+// This program calculates sum of n numbers using recursive approach
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Enter the value of n: " << endl;
+    cin >> n;
+
+    cout << sum(n) << endl;
+
+    return 0;
+}
