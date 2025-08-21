cc7aeb4cc57c08f180399622c33a3fd86f52ad5a
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-22 01:30:37 +0530

    update

--- Binary Search/recursive_method.cpp
+++ Binary Search/recursive_method.cpp
@@ -6,7 +6,22 @@
 using namespace std;
 
 int binarySearch(vector <int> &nums, int target, int start, int end){
-    
+    if(start <= end){
+        int mid = start + ((end - start)/2);
+
+        if(target==nums[mid]){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            return binarySearch(nums, target, start, mid-1);
+        }
+        else{
+            return binarySearch(nums, target, mid+1, end);
+        }
+    }
+    else{
+        return -1;
+    }
 }
 
 int main(){
