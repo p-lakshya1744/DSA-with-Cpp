4ab1eaf4b807bdc47272dcbcd3847a171edc5288
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-27 02:12:20 +0530

    update

--- Graphs/Representation/using_adjacency_matrix.cpp
+++ Graphs/Representation/using_adjacency_matrix.cpp
@@ -43,3 +43,115 @@ int main(){
 
     return 0;
 }
+
+
+// Directed Unweighted Graph
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main(){
+    int vertices, edges;
+
+    cout << "Enter number of vertices: " << endl;
+    cin >> vertices;
+
+    cout << "Enter number of edges: " << endl;
+    cin >> edges;
+
+    vector < vector <bool> > AdjMatrix(vertices, vector <bool> (vertices, 0));
+
+    for(int i=0 ; i<edges ; i++){
+        int u, v;
+        cout << "Enter vertices having edges between them (u, v): " << endl;
+        cin >> u >> v;
+        AdjMatrix[u][v] = 1;
+    }
+
+    for(int i=0 ; i<vertices ; i++){
+        for(int j=0 ; j<vertices ; j++){
+            cout << AdjMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// Undirected Weighted Graph
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main(){
+    int vertices, edges;
+
+    cout << "Enter number of vertices: " << endl;
+    cin >> vertices;
+
+    cout << "Enter number of edges: " << endl;
+    cin >> edges;
+
+    vector < vector <bool> > AdjMatrix(vertices, vector <bool> (vertices, 0));
+
+    for(int i=0 ; i<edges ; i++){
+        int u, v, weight;
+        cout << "Enter vertices having edges between them (u, v): " << endl;
+        cin >> u >> v;
+
+        cout << "Enter weight on the edge: " << endl;
+        cin >> weight;
+        AdjMatrix[u][v] = weight;
+        AdjMatrix[v][u] = weight;
+    }
+
+    for(int i=0 ; i<vertices ; i++){
+        for(int j=0 ; j<vertices ; j++){
+            cout << AdjMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// Directed Weighted Graph
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main(){
+    int vertices, edges;
+
+    cout << "Enter number of vertices: " << endl;
+    cin >> vertices;
+
+    cout << "Enter number of edges: " << endl;
+    cin >> edges;
+
+    vector < vector <bool> > AdjMatrix(vertices, vector <bool> (vertices, 0));
+
+    for(int i=0 ; i<edges ; i++){
+        int u, v, weight;
+        cout << "Enter vertices having edges between them (u, v): " << endl;
+        cin >> u >> v;
+
+        cout << "Enter weight on the edge: " << endl;
+        cin >> weight;
+        AdjMatrix[u][v] = weight;
+    }
+
+    for(int i=0 ; i<vertices ; i++){
+        for(int j=0 ; j<vertices ; j++){
+            cout << AdjMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
