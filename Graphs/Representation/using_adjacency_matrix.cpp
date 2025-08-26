5141ef9cf3599df4692c7d954c7abc70bf069ee0
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-08-27 01:26:26 +0530

    update

--- Graphs/Representation/using_adjacency_matrix.cpp
+++ Graphs/Representation/using_adjacency_matrix.cpp
@@ -1,1 +1,9 @@
-// 
+// This program implements representation of graphs using adjacence matrix
+
+/*
+    Graphs represented:-
+        Undirected Unweighted Graph
+        Directed Unweighted Graph
+        Undirected Weighted Graph
+        Directed Unweighted Graph
+*/
