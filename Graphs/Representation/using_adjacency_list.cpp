ab0b50cac89295ee30cb223a5bd96d97bb755575
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-09-03 16:36:30 +0530

    update

--- Graphs/Representation/using_adjacency_list.cpp
+++ Graphs/Representation/using_adjacency_list.cpp
@@ -102,19 +102,56 @@ int main(){
     cout << "Enter number of edges: " << endl;
     cin >> edges;
 
-    vector <vector <int>> AdjList(vertices);
+    vector <vector <pair <int, int>>> AdjList(vertices);
 
     for(int i=0 ; i<edges ; i++){
-        int u, v;
-        cout << "Enter vertices having edges between them (u, v): " << endl;
-        cin >> u >> v;
-        AdjList[u].push_back(v);
+        int u, v, weight;
+        cout << "Enter vertices having edges between them (u, v) with weight: " << endl;
+        cin >> u >> v >> weight;
+        AdjList[u].push_back(make_pair(v, weight));
+        AdjList[v].push_back(make_pair(u, weight));
     }
 
     for(int i=0 ; i<vertices ; i++){
         cout << i << "->";
         for(int j=0 ; j<AdjList[i].size() ; j++){
-            cout << AdjList[i][j] << " ";
+            cout << AdjList[i][j].first << " " << AdjList[i][j].second << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// Directed Weighted Graph
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main(){
+    int vertices, edges;
+
+    cout << "Enter number of vertices: " << endl;
+    cin >> vertices;
+
+    cout << "Enter number of edges: " << endl;
+    cin >> edges;
+
+    vector <vector <pair <int, int>>> AdjList(vertices);
+
+    for(int i=0 ; i<edges ; i++){
+        int u, v, weight;
+        cout << "Enter vertices having edges between them (u, v) with weight: " << endl;
+        cin >> u >> v >> weight;
+        AdjList[u].push_back(make_pair(v, weight));
+    }
+
+    for(int i=0 ; i<vertices ; i++){
+        cout << i << "->";
+        for(int j=0 ; j<AdjList[i].size() ; j++){
+            cout << AdjList[i][j].first << " " << AdjList[i][j].second << " ";
         }
         cout << endl;
     }
