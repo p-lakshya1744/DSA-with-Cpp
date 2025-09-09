5ff9f3d17fca1f8d930c7df9a1c6ab0e5da1cabe
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-09-10 05:07:33 +0530

    update

--- Graphs/topological_sort.cpp
+++ Graphs/topological_sort.cpp
@@ -6,8 +6,23 @@
 
 using namespace std;
 
-int main(){
+void DFS(int node, vector <vector <int>> adjList, vector <int> isVisited, stack <int> s){
     
+} 
+
+vector <int> topoSort(int v, vector <vector <int>> adjList){
+    vector <int> isVisited(v, 0);
+    stack <int> s;
+
+    DFS(0, adjList, isVisited, s);
+}
+
+int main(){
+    int v = 4;
+
+    vector <vector <int>> adjList = {{3, 0}, {1, 0}, {2, 0}};
+
+    cout << topoSort(v, adjList) << endl;
 
     return 0;
 }
