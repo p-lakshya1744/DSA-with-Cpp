4a6bb05c954051ca58483c3deb3accd950af77ec
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-09-10 05:21:08 +0530

    update

--- Graphs/topological_sort.cpp
+++ Graphs/topological_sort.cpp
@@ -1,20 +1,46 @@
 // This program Topological Sort on Directed Acyclic Graphs (DAGs)
 
+
+// using DFS
+
+// Time Complexity
+// Space Complexity
+
 # include <iostream>
 # include <vector>
 # include <stack>
 
 using namespace std;
 
-void DFS(int node, vector <vector <int>> adjList, vector <int> isVisited, stack <int> s){
-    
+void DFS(int node, vector <vector <int>> adjList, vector <int> &isVisited, stack <int> s){
+    isVisited[node] = 1;
+
+    for(int i=0 ; i<adjList[node].size() ; i++){
+        if(!isVisited[adjList[node][i]]){
+            DFS(adjList[node][i], adjList, isVisited, s);
+        }
+        s.push(adjList[node][i]);
+    }
 } 
 
 vector <int> topoSort(int v, vector <vector <int>> adjList){
     vector <int> isVisited(v, 0);
     stack <int> s;
 
-    DFS(0, adjList, isVisited, s);
+    for(int i=0 ; i<v ; i++){
+        if(!isVisited[i]){
+            DFS(0, adjList, isVisited, s);
+        }
+    }
+
+    vector <int> ans;
+
+    for(int i=0 ; i<s.size() ; i++){
+        ans.push_back(s.top());
+        s.pop();
+    }
+
+    return ans;
 }
 
 int main(){
@@ -22,7 +48,9 @@ int main(){
 
     vector <vector <int>> adjList = {{3, 0}, {1, 0}, {2, 0}};
 
-    cout << topoSort(v, adjList) << endl;
+    for(int val: topoSort(v, adjList)){
+        cout << val << " ";
+    }
 
     return 0;
 }
