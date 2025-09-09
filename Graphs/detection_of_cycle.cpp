625e62d6f641f3e5a53c8eba893030b4756d9155
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-09-09 19:57:11 +0530

    update

--- Graphs/detection_of_cycle.cpp
+++ Graphs/detection_of_cycle.cpp
@@ -5,12 +5,17 @@
 
 using namespace std;
 
-bool detectCycle(int node, int parent, vector <vector <int>> adjList, vector <int> isVisited){
-
+bool detectCycle(int v, vector <vector <int>> adjList){
+    
 }
 
 int main(){
+    int v = 5;
+    int e = 5;
+
+    vector <vector <int>> adjList = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}};
     
+    cout << detectCycle(v, adjList) << endl;
 
     return 0;
 }
