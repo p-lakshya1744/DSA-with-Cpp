042da2bf5426b7767efc223e5e4371944316b2bc
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-09-10 03:06:25 +0530

    update

--- Graphs/Cycle Detection/Undirected Graph/detection_of_cycle.cpp
+++ Graphs/Cycle Detection/Undirected Graph/detection_of_cycle.cpp
@@ -6,51 +6,51 @@
 // Time Complexity - O(V+E)
 // Space Complexity - O(V)
 
-# include <iostream>
-# include <vector>
+// # include <iostream>
+// # include <vector>
 
-using namespace std;
+// using namespace std;
 
-bool detectCycle(int node, int parentNode , vector <vector <int>> adjList, vector <int> &isVisited){
-    isVisited[node]=1;
+// bool detectCycle(int node, int parentNode , vector <vector <int>> adjList, vector <int> &isVisited){
+//     isVisited[node]=1;
 
-    for(int i=0 ; i<adjList[node].size() ; i++){
-        if(node==parentNode){
-            continue;
-        }
-        if(isVisited[adjList[node][i]]==1){
-            return true;
-        }
+//     for(int i=0 ; i<adjList[node].size() ; i++){
+//         if(node==parentNode){
+//             continue;
+//         }
+//         if(isVisited[adjList[node][i]]==1){
+//             return true;
+//         }
 
-        if(detectCycle(adjList[node][i], node, adjList, isVisited)){
-            return true;
-        }
-    }
+//         if(detectCycle(adjList[node][i], node, adjList, isVisited)){
+//             return true;
+//         }
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-bool isCycle(int v, vector <vector <int>> adjList){
-    vector <int> isVisited(v, 0);
+// bool isCycle(int v, vector <vector <int>> adjList){
+//     vector <int> isVisited(v, 0);
 
-    for(int i=0 ; i<v ; i++){
-        if(!isVisited[i] && detectCycle(i, -1, adjList, isVisited)){
-            return true;
-        }
-    }
+//     for(int i=0 ; i<v ; i++){
+//         if(!isVisited[i] && detectCycle(i, -1, adjList, isVisited)){
+//             return true;
+//         }
+//     }
     
-    return false;
-}
+//     return false;
+// }
 
-int main(){
-    int v = 5;
+// int main(){
+//     int v = 5;
 
-    vector <vector <int>> adjList = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}};
+//     vector <vector <int>> adjList = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}};
     
-    cout << isCycle(v, adjList) << endl;
+//     cout << isCycle(v, adjList) << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 // using BFS
@@ -63,7 +63,21 @@ int main(){
 
 using namespace std;
 
+bool detectCycle(int node, int parentNode, vector <vector <int>> adjList, vector <int> isVisited){
 
+}
+
+bool isCycle(int v, vector <vector <int>> adjList){
+    vector <int> isVisited(v, 0);
+
+    for(int i=0 ; i<v ; i++){
+        if(!isVisited[i] && detectCycle(i, -1, adjList, isVisited)){
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main(){
     int v = 5;
