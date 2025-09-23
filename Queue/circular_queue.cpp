17ecc83325a4de99592bcc9781c23ec542a20cdb
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-09-23 14:12:59 +0530

    update

--- Queue/circular_queue.cpp
+++ Queue/circular_queue.cpp
@@ -1,1 +1,72 @@
 // This program implements the Circular Queue using Arrays
+
+# include <iostream>
+using namespace std;
+
+class CircularQueue{
+    public:
+
+    int* arr;
+    int currSize = 0;
+    int cap;
+    int f, rear;
+
+    CircularQueue(int size){
+        cap = size;
+        arr = new int[cap];
+        f = 0;
+        rear = -1;
+    }
+
+    void push(int data){
+        if(currSize==cap){
+            cout << "Circular is Full" << endl;
+            return;
+        }
+        rear = (rear + 1)%cap;
+        arr[rear] = data;
+        currSize++;
+    }
+
+    void pop(){
+        if(currSize==0){
+            cout << "Circular Queue is empty" << endl;
+            return;
+        }
+        f = (f + 1)%cap;
+        currSize--;
+    }
+
+    int front(){
+        if(currSize==0){
+            cout << "Circular Queue is empty" << endl;
+            return -1;
+        }
+        return arr[f];
+    }
+
+    bool empty(){
+        return currSize==0;
+    }
+
+    void printArr(){
+        for(int i=0 ; i<cap ; i++){
+            cout << arr[i] << " ";
+        }
+    }
+
+};
+
+int main(){
+    CircularQueue cq(3);
+
+    cq.push(1);
+    cq.push(2);
+    cq.push(3);
+    cq.pop();
+    cq.push(4);
+
+    cq.printArr();
+
+    return 0;
+}
