d286dbd7abe7c49a3982ffb60b91457dc048ec42
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-10-09 22:22:31 +0530

    update

---
+++ Stacks/next_greater_element.cpp
@@ -0,0 +1,35 @@
+// This program finds the next greater element for any element in a vector
+
+# include <iostream>
+# include <vector>
+# include <stack>
+
+using namespace std;
+
+int main(){
+    vector <int> nums = {6, 8, 0, 1, 3};
+
+    vector <int> ans(nums.size(), 0);
+    stack <int> s;
+
+    for(int i=nums.size()-1 ; i>=0 ; i--){
+        while(s.size()>0 && nums[s.top()]<=nums[i]){
+            s.pop();
+        }
+
+        if(s.empty()){
+            ans[i] = -1;
+        }
+        else{
+            ans[i] = nums[s.top()];
+        }
+
+        s.push(i);
+    }
+
+    for(int val: ans){
+        cout << val << " ";
+    }
+
+    return 0;
+}
