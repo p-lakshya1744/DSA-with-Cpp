3a5246bd5976652a8d8f2b9d897e404b8f4a3974
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-10-11 03:22:20 +0530

    update

---
+++ Stacks/celebrity_problem.cpp
@@ -0,0 +1,9 @@
+/*
+
+Given a 2D-Array (n*n), such that arr[i][j] = 1 means the ith person knows jth person, the task is to find the celebrity.
+A celebrity is a person who is known to all but does not know anyone.
+
+Return the index of the celebrity, if there is no celebrity return -1.
+
+*/
+
