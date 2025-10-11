0e299b9431b31d6bbbf115b0ed7523f55b43093b
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-10-11 16:31:48 +0530

    update

--- Trees/Building a Tree/using_preorder_sequence.cpp
+++ Trees/Building a Tree/using_preorder_sequence.cpp
@@ -5,8 +5,39 @@
 
 using namespace std;
 
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector <int> preorder){
+    idx++;
+
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root -> left = buildTree(preorder);
+    root -> right = buildTree(preorder);
+
+    return root;
+}
+
+
+
 int main(){
-    
+    vector <int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     return 0;
 }
