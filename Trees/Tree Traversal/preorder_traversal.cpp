09eb645efb526616cbb3167280ca731c3798ddf2
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-10-12 18:31:31 +0530

    update

--- Trees/Tree Traversal/preorder_traversal.cpp
+++ Trees/Tree Traversal/preorder_traversal.cpp
@@ -1,12 +1,59 @@
 // This program implements teh preorder traversal on a binary tree
 
+// Time Complexity - O(n)
 # include <iostream>
 # include <vector>
 
 using namespace std;
 
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector <int> preorder){
+    idx++;
+
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root -> left = buildTree(preorder);
+    root -> right = buildTree(preorder);
+
+    return root;
+}
+
+void preorderTraversal(Node* root){
+    if(root==NULL){
+        return ;
+    }
+
+    cout << root -> data << endl;
+    preorderTraversal(root -> left);
+    preorderTraversal(root -> right);
+
+}
+
+
+
 int main(){
-    
+    vector <int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preorder);
+
+    preorderTraversal(root);
 
     return 0;
 }
