e4e01399d6030658643f00d5833489d002794240
Lakshya Patel <lakshyapatel.lp2005@gmail.com>
2025-10-12 18:51:35 +0530

    update

---
+++ Trees/Tree Traversal/level_order_traversal.cpp
@@ -0,0 +1,74 @@
+// This program implements the level order traversal for a binary tree
+// This approach is BFS-based
+
+
+// Time Complexity - O(n)
+
+# include <iostream>
+# include <vector>
+# include <queue>
+
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector <int> preorder){
+    idx++;
+
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root -> left = buildTree(preorder);
+    root -> right = buildTree(preorder);
+
+    return root;
+}
+
+void levelorderTraversal(Node* root){
+    queue <Node*> q;
+
+    q.push(root);
+
+    while(!q.empty()){
+        Node* currNode = q.front();
+        q.pop();
+
+        cout << currNode -> data << endl;
+
+        if(currNode -> left != NULL){
+            q.push(currNode -> left);
+        }
+
+        if(currNode -> right != NULL){
+            q.push(currNode -> right);
+        }
+    }
+
+}
+
+
+
+int main(){
+    vector <int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preorder);
+
+    levelorderTraversal(root);
+
+    return 0;
+}
